9216586fc041fc4e9b5b75fb18d4c56d31563f88
samuelmecham <142920339+samuelmecham@users.noreply.github.com>
2025-11-10 10:04:27 -0700

    DAY1

---
+++ script.js
@@ -0,0 +1,15 @@
+let pasword = document.getElementById(password)
+
+// checkboxes 
+let upercase = document.getElementById(uppercase)
+let numbers = document.getElementById(numbers)
+let symbles = document.getElementById(symbols)
+
+//options
+let optionArray = [];
+let lowercase = [
+    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
+]
+let uppercase = [
+    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+]
